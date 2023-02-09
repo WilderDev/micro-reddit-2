5ee97d9f3c9edf6efe8e84240107df056e3dc3c4
WilderDev <williamjohnwilder@outlook.com>
2023-02-09 09:49:24 -0600

    User view

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show]
   resources :communities, only: [:index, :new, :create, :show]
+  resources :posts
+  resources :comments, only: [:create]
 end
